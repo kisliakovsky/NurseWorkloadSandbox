254ec49552fe3b0b83a125be15df540bf9226e97
kisliakovsky <kisliakovskiii@yandex.ru>
2018-03-01 15:41:05 +0300

    main() added.

---
+++ src/main/java/ru/ifmo/escience/compbiomed/sandbox/Experiment.java
@@ -0,0 +1,21 @@
+package ru.ifmo.escience.compbiomed.sandbox;
+
+import ru.ifmo.escience.compbiomed.sandbox.block.NurseSource;
+import ru.ifmo.escience.compbiomed.sandbox.simulation.AbstractEvent;
+import ru.ifmo.escience.compbiomed.sandbox.simulation.Simulation;
+
+public class Experiment {
+
+    public static void main(String[] args) {
+        final Simulation simulation = new Simulation(25.0);
+        final NurseSource source = new NurseSource();
+        simulation.addEvent(new AbstractEvent(1.1) {
+            @Override
+            public void execute() {
+                source.inject(1);
+            }
+        });
+        simulation.run();
+    }
+
+}
